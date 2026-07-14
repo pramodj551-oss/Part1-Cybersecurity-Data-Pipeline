8b5b4b40c83d69f52a2485016710323982c1421c
Pramod prakash jadhav <pramodj551@gmail.com>
2026-07-14 16:36:58 +0530

    Create schema.py

---
+++ src/schema.py
@@ -0,0 +1,17 @@
+REQUIRED_COLUMNS = [
+    "incident_id",
+    "incident_date",
+    "attack_type",
+    "sector",
+    "region",
+    "severity_score"
+]
+
+OPTIONAL_COLUMNS = [
+    "records_affected",
+    "downtime_hours",
+    "ransom_demand_usd",
+    "regulatory_fine_usd",
+    "response_team_size",
+    "threat_actor"
+]
