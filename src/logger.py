862c2df729fdcdf1356418bbaec946452e9794cd
Pramod prakash jadhav <pramodj551@gmail.com>
2026-07-14 16:42:10 +0530

    Update and rename schema.py to logger.py

---
+++ src/logger.py
@@ -0,0 +1,33 @@
+"""
+Central Logger Configuration
+"""
+
+from __future__ import annotations
+
+import logging
+
+from src.config import LOG_FILE
+
+logger = logging.getLogger("CyberAnalytics")
+
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+
+    formatter = logging.Formatter(
+
+        "%(asctime)s | %(levelname)s | %(message)s"
+
+    )
+
+    file_handler = logging.FileHandler(LOG_FILE)
+
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
