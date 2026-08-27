049cb88557660c4b24308ddb06dc3404b32a5218
Pramod prakash jadhav <pramodj551@gmail.com>
2026-08-27 12:01:14 +0530

    fix: ensure log directory exists

--- src/logger.py
+++ src/logger.py
@@ -13,21 +13,16 @@ logger = logging.getLogger("CyberAnalytics")
 logger.setLevel(logging.INFO)
 
 if not logger.handlers:
+    LOG_FILE.parent.mkdir(parents=True, exist_ok=True)
 
     formatter = logging.Formatter(
-
         "%(asctime)s | %(levelname)s | %(message)s"
-
     )
 
     file_handler = logging.FileHandler(LOG_FILE)
-
     file_handler.setFormatter(formatter)
-
     logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
-
     console_handler.setFormatter(formatter)
-
     logger.addHandler(console_handler)
